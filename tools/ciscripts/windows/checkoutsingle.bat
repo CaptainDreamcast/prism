54cfaf4109f6dff68c8b31f1506dcc30982d1a92
Josh Tari <josh.tari@dogmacompany.com>
2017-05-26 23:01:46 +0200

    [Feature] add building to checkout script

--- tools/ciscripts/windows/checkoutsingle.bat
+++ tools/ciscripts/windows/checkoutsingle.bat
@@ -1,5 +1,8 @@
 REM Expects _libtari_index index and setprojects to have been called before
 
 set _libtari_projectpath=..\..\..\..\..\!_libtari_projects[%_libtari_index%]!
+set _libtari_solpath=%_libtari_projectpath%\windows\!_libtari_projects[%_libtari_index%]!.sln
+
 git clone !_libtari_repos[%_libtari_index%]!
 move !_libtari_projects[%_libtari_index%]! %_libtari_projectpath%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" %_libtari_solpath% /t:build /p:Configuration=Release;Platform=x86
