716180d9a8c5220aae211dcbf2a44ffe6b35ce99
Josh Tari <josh.tari@dogmacompany.com>
2018-04-02 18:25:51 +0200

    [Feature] add blitz header

---
+++ include/prism/blitz.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "log.h"
+#include "system.h"
+#include "math.h"
+#include "wrapper.h"
+#include "geometry.h"
+
+#include "blitzcamerahandler.h"
+#include "blitzcollision.h"
+#include "blitzcomponent.h"
+#include "blitzentity.h"
+#include "blitzmugenanimation.h"
+#include "blitzmugensound.h"
+#include "blitzparticles.h"
+#include "blitzphysics.h"
+#include "blitztimelineanimation.h"
