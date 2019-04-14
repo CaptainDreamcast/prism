262648962aabe490bba2b9da1a2d94fcf0f79ac7
Josh Tari <josh.tari@dogmacompany.com>
2019-04-14 07:42:17 -0700

    [Bugfix] more conservative PVR parameters

--- dc/pvr_dc.cpp
+++ dc/pvr_dc.cpp
@@ -5,7 +5,24 @@
 #include <prism/debug.h>
 
 void initiatePVR() {
-	pvr_init_defaults();
+	pvr_init_params_t params = {
+        /* Enable opaque and translucent polygons with size 16 */
+        { PVR_BINSIZE_0, PVR_BINSIZE_0, PVR_BINSIZE_16, PVR_BINSIZE_0, PVR_BINSIZE_0 },
+
+        /* Vertex buffer size 192K */
+        192 * 1024,
+
+        /* No DMA */
+        0,
+
+        /* No FSAA */
+        0,
+
+        /* Translucent Autosort enabled. */
+        0
+    };
+
+    pvr_init(&params);
 }
 
 void drawPVRDebuggingBorder(double r, double g, double b) {
