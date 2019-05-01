638c1d6fff8eff1fc5ef05260e5b5519d763b86c
Josh Tari <josh.tari@dogmacompany.com>
2019-05-01 21:42:01 +0200

    [Bugfix] fix powershell path in template

--- assets/template/windows/pre_build.bat
+++ assets/template/windows/pre_build.bat
@@ -1,6 +1,6 @@
 ..\..\tools\genromfs-win\x64\Debug\genromfs.exe -d ..\assets -f %1\assets.pak
 ..\..\tools\bin2c\x64\Debug\bin2c.exe %1\assets.pak ..\assets.cpp romdisk_buffer
 set cur_file=..\assets.cpp
-powershell -Command "(gc %cur_file%) -replace 'const char', 'char' | Out-File %cur_file%"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "(gc %cur_file%) -replace 'const char', 'char' | Out-File %cur_file%"
 set cur_file=..\assets.cpp
-powershell -Command "(gc %cur_file%) -replace 'const int', 'int' | Out-File %cur_file%"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "(gc %cur_file%) -replace 'const int', 'int' | Out-File %cur_file%"
