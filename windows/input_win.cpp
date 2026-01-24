98e3a9ed7522cf072b36fd96e86f18153b7a559d
Josh Tari <josh.tari@dogmacompany.com>
2026-01-25 00:23:30 +0100

    [Release] Why Can't We Be Friends

--- windows/input_win.cpp
+++ windows/input_win.cpp
@@ -560,13 +560,13 @@ namespace prism {
 	static void updateKeyboardLocalKeyboard(int i) {
 		updateKeyStateArraySize(i);
 #ifdef _WIN32
-		bool wantTextInput = true;
+		bool wantTextInput = false;
 		if (isImguiPrismActive())
 		{
 			auto& io = ImGui::GetIO();
 			wantTextInput = io.WantTextInput;
 		}
-		if (wantTextInput)
+		if (!wantTextInput)
 #endif
 		{
 			memcpy(gPrismWindowsInputData.mKeyboards[i].mKeyStates.front().data(), gPrismWindowsInputData.mKeyboards[i].mKeyStatePointer, SDL_NUM_SCANCODES);
